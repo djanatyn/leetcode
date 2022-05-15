6bef7e62893699b6dfd61aaa2f2f1ed876946f65
Jonathan Strickland <djanatyn@gmail.com>
2022-05-15 16:11:04 -0400

    common lisp solution for next-greater-element-i

---
+++ next-greater-element-i/solve.lisp
@@ -0,0 +1,29 @@
+(defparameter *example-1*
+  '((nums1 . (4 1 2))
+    (nums2 . (1 3 4 2))
+    (answer . (-1 3 -1))))
+
+(defparameter *example-2*
+  '((nums1 . (2 4))
+    (nums2 . (1 2 3 4))
+    (answer . (3 -1))))
+
+(defun next-greater-element (element sequence)
+  (let* ((element-position (position element sequence))
+         (right-elements (nthcdr (+ 1 element-position) sequence))
+         (greater-elements (remove-if (lambda (n) (<= n element)) right-elements)))
+    (cond ((null greater-elements) -1)
+          (t (first greater-elements)))))
+
+(defun solve (nums1 nums2)
+  (mapcar (lambda (element) (next-greater-element element nums2)) nums1))
+
+(defun solve-example (example)
+  (let ((nums1 (rest (assoc 'nums1 example)))
+        (nums2 (rest (assoc 'nums2 example)))
+        (answer (rest (assoc 'answer example))))
+    (solve nums1 nums2)))
+
+(defun solve-examples ()
+  `((example-1 . ,(solve-example *example-1*))
+    (example-2 . ,(solve-example *example-2*))))
