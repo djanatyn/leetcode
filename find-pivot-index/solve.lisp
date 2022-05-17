4c819374c5c7c3fc871128429398fba82cd1c760
Jonathan Strickland <djanatyn@gmail.com>
2022-05-16 22:40:26 -0400

    find-pivot-index: add example vectors

---
+++ find-pivot-index/solve.lisp
@@ -0,0 +1,13 @@
+;; https://leetcode.com/problems/find-pivot-index/
+
+(defparameter *example-1*
+  '((nums . #(1 7 3 6 5 6))
+    (output . 3)))
+
+(defparameter *example-2*
+  '((nums . #(1 2 3))
+    (output . -1)))
+
+(defparameter *example-3*
+  '((nums . #(2 1 -1))
+    (output . 0)))
